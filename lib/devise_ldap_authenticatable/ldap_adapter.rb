4efa160036811b3a2f391c16c4cd8bb88ced7ee4
Corey Purcell <corey_purcell@pennunited.com>
2010-05-25 14:41:20 -0400

    authenticate using active directory

--- lib/devise_ldap_authenticatable/ldap_adapter.rb
+++ lib/devise_ldap_authenticatable/ldap_adapter.rb
@@ -7,17 +7,31 @@ module Devise
   module LdapAdapter
 
     def self.valid_credentials?(login, password)
-      login = ::Devise.ldap_login_attribute+'='+login+','+::Devise.ldap_base_dn      
+      unless ::Devise.active_directory
+        login = ::Devise.ldap_login_attribute+'='+login+','+::Devise.ldap_base_dn      
+      end
       @encryption = ::Devise.ldap_ssl ? :simple_tls : nil
       ldap = Net::LDAP.new(:encryption => @encryption)
       ldap.host = ::Devise.ldap_host
       ldap.port = ::Devise.ldap_port
-      ldap.auth login, password
-      if ldap.bind
-        true
+      if ::Devise.active_directory
+        ldap.authenticate(::Devise.auth_user, ::Devise:auth_password)
+        if ldap.bind_as(:base => ::Devise.ldap_base_dn, 
+                     :password => password, 
+                     :filter => Net::LDAP::Filter.eq('sAMAccountName',login))
+          true
+        else
+          false
+        end
       else
-        # errors.add_to_base(ldap.get_operation_result.message)
-        false
+        ldap.auth login, password
+      
+        if ldap.bind
+          true
+        else
+          # errors.add_to_base(ldap.get_operation_result.message)
+          false
+        end
       end
     end
 
